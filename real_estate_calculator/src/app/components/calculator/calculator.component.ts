7ceb70d70ca22ee0aec681fc637056c522344cfc
danielyordanov07 <98211151+danielyordanov07@users.noreply.github.com>
2024-12-12 15:10:35 +0200

    Added usable ts properties

--- real_estate_calculator/src/app/components/calculator/calculator.component.ts
+++ real_estate_calculator/src/app/components/calculator/calculator.component.ts
@@ -1,19 +1,29 @@
-import { Component} from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, input} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
 export class MainCalculatorComponent {
-  // init array
-  values: number[] = [0, 0, 0, 0, 0];
+
+  values: { value1: number; value2: number; value3: number; value4: number; value5: number; value6: number; value7: number } = {
+    value1: 0,
+    value2: 0,
+    value3: 0,
+    value4: 0,
+    value5: 0,
+    value6: 0,
+    value7: 0
+  };
   result: number = 0;
 
-  calculate(number: any[]): number {
-    return 0;
+  calculate(): number {
+
+    return this.result;
   }
 }
+
