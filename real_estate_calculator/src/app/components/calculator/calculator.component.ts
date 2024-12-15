8379359c6a12f7a8a496a8b739c447cba257158d
danielyordanov07 <98211151+danielyordanov07@users.noreply.github.com>
2024-12-15 20:12:49 +0200

    implement bootstrap, added calculations, remove use of ngOnChanges

--- real_estate_calculator/src/app/components/calculator/calculator.component.ts
+++ real_estate_calculator/src/app/components/calculator/calculator.component.ts
@@ -1,29 +1,50 @@
-import { Component, input} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'calculator',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
-export class MainCalculatorComponent {
+export class MainCalculatorComponent implements OnInit {
 
-  values: { value1: number; value2: number; value3: number; value4: number; } = {
-    value1: 0,
-    value2: 0,
-    value3: 0,
-    value4: 0,
+  inputs: { salePrice: number; repairCosts: number; apr: number; loanYears: number } = {
+    salePrice: 0,
+    repairCosts: 0,
+    apr: 0,
+    loanYears: 0,
   };
 
-  results: { result1: number; result2: number; result3: number } = {
-    result1: 0,
-    result2: 0,
-    result3: 0
+  results: { purchaseCosts: number; credit: number; monthlyRate: number; months: number, monthlyPayment: number } = {
+    purchaseCosts: 0,
+    credit: 0,
+    monthlyRate: 0,
+    months: 0,
+    monthlyPayment: 0,
   };
 
-  
+  public ngOnInit(): void {
+    console.log('Initialized');
+  }
+
+  public calculateResults(): void {
+    this.results.purchaseCosts = this.inputs.salePrice * 1.07;
+    this.results.credit = this.results.purchaseCosts + this.inputs.repairCosts;
+    
+    this.results.monthlyRate = (this.inputs.apr / 100) / 12;;
+    this.results.months = this.inputs.loanYears * 12;
+
+    if (this.results.monthlyRate > 0) {
+      this.results.monthlyPayment = this.results.credit * 
+        (this.results.monthlyRate * Math.pow(1 + this.results.monthlyRate, this.results.months)) /
+        (Math.pow(1 + this.results.monthlyRate, this.results.months) - 1);
+    } else {
+      this.results.monthlyPayment = this.results.credit / this.results.months;
+    }
+
+  }
 
 }
 
